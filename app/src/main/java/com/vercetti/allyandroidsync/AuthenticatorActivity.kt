366c582cab2030f4ea4939b8311bb41b801ba741
Samuel Baraka <sababuvercetti@gmail.com>
2020-03-28 00:55:12 +0300

    Added authenticator activity

---
+++ app/src/main/java/com/vercetti/allyandroidsync/AuthenticatorActivity.kt
@@ -0,0 +1,12 @@
+package com.vercetti.allyandroidsync
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authenticator)
+    }
+}
