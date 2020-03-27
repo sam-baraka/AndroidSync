dd896e850d96317bd65cc6617ea3810aa1c60cae
Samuel Baraka <sababuvercetti@gmail.com>
2020-03-28 00:52:48 +0300

    Progress

--- app/src/main/java/com/vercetti/allyandroidsync/AuthenticatorService.kt
+++ app/src/main/java/com/vercetti/allyandroidsync/AuthenticatorService.kt
@@ -1,19 +1,76 @@
 package com.vercetti.allyandroidsync
 
-import android.app.Service
+import android.accounts.*
+import android.content.Context
 import android.content.Intent
-import android.os.IBinder
+import android.os.Bundle
+import android.provider.SyncStateContract
 
 
-class AuthenticatorService : Service() {
-
-    private var authenticator: AccountAuthenticator? = null
-
-    override fun onCreate() {
-        authenticator = AccountAuthenticator(this)
+class Authenticator(context: Context) : AbstractAccountAuthenticator(context) {
+    private val mContext: Context
+    override fun editProperties(
+        response: AccountAuthenticatorResponse,
+        accountType: String
+    ): Bundle? {
+        return null
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return authenticator!!.iBinder
+    @Throws(NetworkErrorException::class)
+    override fun addAccount(
+        response: AccountAuthenticatorResponse,
+        accountType: String,
+        authTokenType: String,
+        requiredFeatures: Array<String>,
+        options: Bundle
+    ): Bundle {
+        val intent = Intent(mContext, AuthenticatorActivity::class.java)
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+        val bundle = Bundle()
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent)
+        return bundle
+    }
+
+    @Throws(NetworkErrorException::class)
+    override fun confirmCredentials(
+        response: AccountAuthenticatorResponse,
+        account: Account, options: Bundle
+    ): Bundle? {
+        return null
+    }
+
+    @Throws(NetworkErrorException::class)
+    override fun getAuthToken(
+        response: AccountAuthenticatorResponse, account: Account,
+        authTokenType: String, options: Bundle
+    ): Bundle {
+        val result = Bundle()
+        result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name)
+        result.putString(AccountManager.KEY_ACCOUNT_TYPE, SyncStateContract.Constants.ACCOUNT_TYPE)
+        return result
+    }
+
+    override fun getAuthTokenLabel(authTokenType: String): String? {
+        return null
+    }
+
+    @Throws(NetworkErrorException::class)
+    override fun updateCredentials(
+        response: AccountAuthenticatorResponse, account: Account,
+        authTokenType: String, options: Bundle
+    ): Bundle? {
+        return null
+    }
+
+    @Throws(NetworkErrorException::class)
+    override fun hasFeatures(
+        response: AccountAuthenticatorResponse, account: Account,
+        features: Array<String>
+    ): Bundle? {
+        return null
+    }
+
+    init {
+        mContext = context
     }
 }
